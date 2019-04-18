fc146656f2c77c1c34d2aa6893e0b007e6e8337e
Andi Péter <andipeti2000@gmail.com>
2019-04-18 10:34:53 +0200

    no global i

--- assets/spoiler.js
+++ assets/spoiler.js
@@ -1,7 +1,6 @@
 let spoilers = document.getElementsByClassName("spoiler-collapse");
-var i;
 
-for (i = 0; i < spoilers.length; i++) {
+for (let i = 0; i < spoilers.length; i++) {
   spoilers[i].addEventListener("click", function() {
     this.classList.toggle("spoiler-collapse-active");
     var content = this.nextElementSibling;
