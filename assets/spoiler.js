ea6aecac833eb525cab02bbe9c359316b00faea3
Andi Péter <andipeti2000@gmail.com>
2019-04-18 10:31:01 +0200

    Typo

--- assets/spoiler.js
+++ assets/spoiler.js
@@ -1,7 +1,7 @@
 let spoilers = document.getElementsByClassName("spoiler-collapse");
 var i;
 
-for (i = 0; i < colls.length; i++) {
+for (i = 0; i < spoilers.length; i++) {
   spoilers[i].addEventListener("click", function() {
     this.classList.toggle("spoiler-collapse-active");
     var content = this.nextElementSibling;
