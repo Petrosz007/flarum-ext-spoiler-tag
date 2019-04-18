4d245784add1e3067c4abacb48642169ad361275
Andi Péter <andipeti2000@gmail.com>
2019-04-18 12:18:09 +0200

    Changed to JQuery

--- extend.php
+++ extend.php
@@ -18,23 +18,12 @@ return [
         ->css(__DIR__ . '/assets/style.css')
         ->content(function (Document $document) {
             $document->head[] = "<script>
-                                    setTimeout(() => {
-                                        let spoilers = document.getElementsByClassName('spoiler-collapse');
-
-                                        for(let index_ = 0; index_ < spoilers.length; index_++) {
-                                            let element = spoilers[index_];
-                                            element.addEventListener('click', (e) => {
-                                                e.currentTarget.classList.toggle('spoiler-collapse-active');
-
-                                                let spoiler_content = e.currentTarget.nextElementSibling;
-                                                if(spoiler_content.style.maxHeight != 0) {
-                                                    spoiler_content.style.maxHeight = 0;
-                                                } else {
-                                                    spoiler_content.style.maxHeight = spoiler_content.scrollHeight + 'px';
-                                                }
-                                            });
-                                        }
-                                    }, 1000);
+                                    $(document).ready(function() {
+                                        $('spoiler-toggle').click(function(){
+                                            $(this).toggleClass('spoiler-toggle-active');
+                                            $(this).parent().next('div.spoiler-content').slideToggle('slow');
+                                        });
+                                    });
                                 </script>";
         }),
     (new Extend\Formatter)
@@ -42,7 +31,7 @@ return [
             $config->BBCodes->addCustom(
                 '[spoiler]{TEXT1}[/spoiler]',
                 '<div class="spoiler">
-                    <button class="spoiler-collapse">SPOILER</button>
+                    <button class="spoiler-toggle">SPOILER</button>
                     <div class="spoiler-content">
                         <p>
                         {TEXT1}
