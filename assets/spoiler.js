b57c19d296433b8dea26756e6072e45d54c295e2
Andi Péter <andipeti2000@gmail.com>
2019-04-18 11:03:30 +0200

    Fixed 'this' inside the event listener

--- assets/spoiler.js
+++ assets/spoiler.js
@@ -2,10 +2,10 @@ let spoilers = document.getElementsByClassName('spoiler-collapse');
 
 for(let index_ = 0; index_ < spoilers.length; index_++) {
     let element = spoilers[index_];
-    element.addEventListener('click', () => {
-        this.classList.toggle('spoiler-collapse-active');
+    element.addEventListener('click', (e) => {
+        e.currentTarget.classList.toggle('spoiler-collapse-active');
 
-        let spoiler_content = this.nextElementSibling;
+        let spoiler_content = e.currentTarget.nextElementSibling;
         if(spoiler_content.style.maxHeight == 0) {
             spoiler_content.style.maxHeight = 0;
         } else {
